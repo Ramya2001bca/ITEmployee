07601d59288d1811b340eaff26d7801162389558
Ramya2001bca <113408822+Ramya2001bca@users.noreply.github.com>
2022-09-28 13:44:20 +0530

    Add files via upload

---
+++ sasi.java
@@ -0,0 +1,13 @@
+class sasi{
+public static void main(String[] args){
+ITEmployee ramya=new ITEmployee();
+ramya.name="sobhika";
+ramya.age=22;
+//ramya.name2="sasi";
+System.out.println(ramya.name);
+System.out.println(ramya.age);
+//System.out.println(ramya.name2);
+}
+}
+
+
